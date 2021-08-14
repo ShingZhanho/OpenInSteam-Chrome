339eb1bd9c2bbbe370143381a6433cc18439e721
Zhanho Shing <zhanhoshing@icloud.com>
2021-08-14 23:01:36 +0800

    feat: added button tracker

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// start tracking
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'G-2B8DVYDL94']);
+_gaq.push(['_trackPageview']);
+
+function trackButton(btn) {
+    _gaq.push(['_trackEvent', btn.target.id, 'clicked']);
+}
+
 function getContainer() {
     // create container
     var btn_container = document.createElement('div');
@@ -7,6 +16,9 @@ function getContainer() {
     var btn_open_in_client = document.createElement('a');
     btn_open_in_client.className = "btnv6_blue_hoverfade medium_btn";
     btn_open_in_client.setAttribute('href', 'steam://openurl/'.concat(document.URL));
+    btn_open_in_client.setAttribute('id', 'ext_btnOIC');
+    btn_open_in_client.addEventListener('click', trackButton);
+    
     var span_open_in_client = document.createElement('span');
     span_open_in_client.className = 'btn-text';
     span_open_in_client.innerHTML = 'Open in Steam Client';
