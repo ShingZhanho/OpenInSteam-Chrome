9458f6b7f5e0130b4e3b682a6bafe2941621c865
Zhanho Shing <zhanhoshing@icloud.com>
2021-08-14 21:04:44 +0800

    feat: add functionality to button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getContainer() {
     // create open in steam button
     var btn_open_in_client = document.createElement('a');
     btn_open_in_client.className = "btnv6_blue_hoverfade medium_btn";
-    btn_open_in_client.attributes['href'] = 'steam://openurl/'.concat(document.URL);
+    btn_open_in_client.setAttribute('href', 'steam://openurl/'.concat(document.URL));
     var span_open_in_client = document.createElement('span');
     span_open_in_client.className = 'btn-text';
     span_open_in_client.innerHTML = 'Open in Steam Client';
